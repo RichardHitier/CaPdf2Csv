f54a969560ee66b8366d2da79c214df6014d6a21
Richard Hitier <richard.hitier@co-libri.org>
2016-04-13 10:32:04 +0200

    rename files in subdir

--- rename.sh
+++ rename.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+[ -z "$1" ] && { echo "give dir as arg"; exit ;}
+[ -d "$1" ] || { echo "$1 not dir"; exit;}
+
+cd $1
 
 rename 's/Relev_(.*)/Compte_36016373000_Relevé$1/g' *
 rename 's/%20/_/g' *
