c0f0e8dd6447c6559b2c5d03fdb0c08e9b199c86
Richard Hitier <richard.hitier@co-libri.org>
2016-10-18 21:53:47 +0200

    add field separator and build date

--- seconddraw.sh
+++ seconddraw.sh
@@ -1,35 +1,23 @@
 #!/bin/sh
 
-help()
-{
-    echo $1
-    cat << .END
-    The help of the $0
-.END
-}
+file=`echo $1 | egrep 'Compte.*Rele.*_n_[0-9][0-9][0-9]_'` 
 
+if [ -z "$file" ]
+then
+    exit
+fi
 
-while getopts "hdl:" flag
-do
-    case "$flag" in
-        d) BIN="echo BIN";;
-        h) help;;
-        l) 
-            exec >> $OPTARG
-            exec 2>> $OPTARG
-            echo
-            echo "-----------------------------"
-            date
-            echo "-----------------------------"
-         ;;
-    esac
-done
+year=2014
+count=36016373000
 
 pdftotext -layout $1 - |\
-egrep '^ *[0-9][0-9]\.[0-9][0-9] '  |\
-sed -e "s/^ *//" |\
-sed -e "s/^\([0-9][0-9]\.[0-9][0-9]\) *\([0-9][0-9]\.[0-9][0-9]\) */\1/" |\
-sed -e "s/\([0-9][0-9]\.[0-9][0-9]\) */\1 /"
-grep -v yahaha
+    egrep '^ *[0-9][0-9]\.[0-9][0-9] '  |\
+    sed -e "s/^ *//" |\
+    sed -e "s/^\([0-9][0-9]\.[0-9][0-9]\) *\([0-9][0-9]\.[0-9][0-9]\) */\1/" |\
+    sed -e "s/\([0-9][0-9]\)\.\([0-9][0-9]\) */\1\/\2\/$year /" |\
+    sed -r -e 's/^.{66}/&;/'  -e's/^.{11}/&;/' |\
+    sed -e "s/.*/$count;\0/"
+
+
 
 # vim: set tw=0
