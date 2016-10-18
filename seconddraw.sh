05f8a34bda82baea0c608db1fb63e298f38a29d4
Richard Hitier <richard.hitier@co-libri.org>
2016-10-18 22:01:29 +0200

    get year and count from file name

--- seconddraw.sh
+++ seconddraw.sh
@@ -7,8 +7,9 @@ then
     exit
 fi
 
-year=2014
-count=36016373000
+year=`echo $file |cut -d"_" -f9`
+count=`echo $file |cut -d"_" -f2`
+
 
 pdftotext -layout $1 - |\
     egrep '^ *[0-9][0-9]\.[0-9][0-9] '  |\
@@ -16,7 +17,7 @@ pdftotext -layout $1 - |\
     sed -e "s/^\([0-9][0-9]\.[0-9][0-9]\) *\([0-9][0-9]\.[0-9][0-9]\) */\1/" |\
     sed -e "s/\([0-9][0-9]\)\.\([0-9][0-9]\) */\1\/\2\/$year /" |\
     sed -r -e 's/^.{66}/&;/'  -e's/^.{11}/&;/' |\
-    sed -e "s/.*/$count;\0/"
+    sed -e "s/.*/$count;\0/" >> ${year}.csv
 
 
 
