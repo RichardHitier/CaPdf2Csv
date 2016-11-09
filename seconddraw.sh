fcd1a15efb0b7329478af8f76440b981705586e2
Richard Hitier <richard.hitier@co-libri.org>
2016-11-09 16:40:49 +0100

    More transformations:
      . align columns
      . add '-'
      . etc ...

--- seconddraw.sh
+++ seconddraw.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
+#
+# Transform pdf files from the bank
+# to .csv file importable to skrooge
 
+
+# guess if we are dealing with the second pdf format
+#
 file=`echo $1 | egrep 'Compte.*Rele.*_n_[0-9][0-9][0-9]_'` 
 
 if [ -z "$file" ]
@@ -7,18 +13,34 @@ then
     exit
 fi
 
+# set some vars
+#
 year=`echo $file |cut -d"_" -f9`
-count=`echo $file |cut -d"_" -f2`
+account=`echo $file |cut -d"_" -f2`
 
 
-pdftotext -layout $1 - |\
-    egrep '^ *[0-9][0-9]\.[0-9][0-9] '  |\
-    sed -e "s/^ *//" |\
-    sed -e "s/^\([0-9][0-9]\.[0-9][0-9]\) *\([0-9][0-9]\.[0-9][0-9]\) */\1/" |\
-    sed -e "s/\([0-9][0-9]\)\.\([0-9][0-9]\) */\1\/\2\/$year /" |\
-    sed -r -e 's/^.{66}/&;/'  -e's/^.{11}/&;/' |\
-    sed -e "s/.*/$count;\0/" 
+# Transformations:
+
+# 0. to text
+pdftotext -layout $1 - |
+# 1. only get lines with proper date format at beginning
+egrep '^ *[0-9][0-9]\.[0-9][0-9] '  |
+# 2. then get rid of trailing spaces
+sed -e "s/^ *//" |
+# 3. keep only one of the 2 dates
+sed -e "s/^\([0-9][0-9]\.[0-9][0-9]\) *\([0-9][0-9]\.[0-9][0-9]\) */\1 /" |
+# 4. make proper date (TODO: year-1 if month = 1 && filenum == 001 )
+sed -e "s/\([0-9][0-9]\)\.\([0-9][0-9]\) */\1\/\2\/$year /" |
+# 5. add ';' as field separator
+sed -r -e 's/^.{66}/&;/'  -e 's/^.{11}/&;/' |
+# 6. get rid of trailing chars and align last column
+sed -e "s/ *¨$//"  -e "s/; *\([^;]*\)$/; \1/" |
+# 7. '-' everywhere unless if "Virement" or "Rem Chq"
+sed -e "/\(Virement\|Rem Chq\)/!s/; *\([^;]*\)$/;-\1/" |
+# 8. prepend with account number
+sed -e "s/.*/$account;\0/" |
+cat
 
 
 
-# vim: set tw=0
+# vim: tw=0
